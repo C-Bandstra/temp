9dfdc78ec46a00ca521eb22088568c8809dc27a4
C-Bandstra <bandstracharlie@gmail.com>
2021-01-06 13:22:46 -0700

    Create store and load module (Auth bp setup)

--- aetheria-webapp/src/store/store.js
+++ aetheria-webapp/src/store/store.js
@@ -1,11 +1,14 @@
 import { createStore } from 'vuex'
+import patients from './modules/patients'
+// import Auth from './modules/Auth'
 
 const store = createStore({
-  state () {
-    return {
-      patients: []
-    }
+  modules: {
+    patients
+    // auth: Auth,
   }
 })
+
+store.state.patients
 
 export default store;
