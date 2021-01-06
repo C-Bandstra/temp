40a7de4d37f2218d45e87b29213d619efcd77bee
C-Bandstra <bandstracharlie@gmail.com>
2021-01-06 13:38:49 -0700

    Remove test data

--- aetheria-webapp/src/store/modules/patients.js
+++ aetheria-webapp/src/store/modules/patients.js
@@ -5,41 +5,7 @@ const Patients = {
     state: () => {
         return {
             patients: {
-                "therapist_id": 2,
-                "patients": [
-                    {
-                        "patient_name": "Robert Davis",
-                        "patient_id": 1,
-                        "intake_date": "12/18/2020",
-                        "coping_stat": 1.21,
-                        "severity_stat": 7.83,
-                        "median_health_stat": 4.8
-                    },
-                    {
-                        "patient_name": "Cindy Castillo",
-                        "patient_id": 2,
-                        "intake_date": "12/18/2020",
-                        "coping_stat": 1.83,
-                        "severity_stat": 7.15,
-                        "median_health_stat": 5.03
-                    },
-                    {
-                        "patient_name": "Timothy Castillo",
-                        "patient_id": 3,
-                        "intake_date": "12/18/2020",
-                        "coping_stat": 6.04,
-                        "severity_stat": 1.79,
-                        "median_health_stat": 5.23
-                    },
-                    {
-                        "patient_name": "Allison Evans",
-                        "patient_id": 4,
-                        "intake_date": "12/18/2020",
-                        "coping_stat": 9.2,
-                        "severity_stat": 1.94,
-                        "median_health_stat": 8.16
-                    }
-                ]
+                
             }
         }
     },
